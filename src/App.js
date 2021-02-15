5c0ef1ce38c3ce609c1afc875fc99febf9fe07a7
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-15 15:39:33 -0600

    my first react code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+// if node
+// const HelloCullen = require('./HelloCullen');
+//in react
+import HelloCullen from './HelloCullen';
+
+// app is what we call a component
+// react is made of components
+// components are functions that return JSX = HTML
 function App() {
+  // embed variables in the HTML
+  let name = 'Edan';
+
+  let myH1 = <h1>go home, {name}, youre drunk!!!!</h1>;
   return (
     <div className="App">
+      {myH1}
+      {/* HelloCullen is a React Compoennt */}
       <header className="App-header">
+        <HelloCullen /> {/** self closing tag */}
+        <HelloCullen />
+        <HelloCullen />
+        <HelloCullen />
+        <HelloCullen />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload and save to win
         </p>
         <a
           className="App-link"
@@ -21,5 +40,9 @@ function App() {
     </div>
   );
 }
+
+// function HelloCullen() {
+//   return <h1>Hello Cullen</h1>;
+// }
 
 export default App;
---
+++ src/HelloCullen.jsx
@@ -0,0 +1,12 @@
+function HelloCullen() {
+  return (
+    <>
+      <h1>Hello Cullen</h1>
+      <h2>how you doing</h2>
+    </>
+  );
+}
+
+// in node module.exports
+// in react
+export default HelloCullen;
