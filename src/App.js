67f500e724fcae4500c6e77a41de41e69769397c
John Anthony <jmanthony.fullsail@gmail.com>
2021-02-15 16:06:33 -0600

    learned about useStates and how to set them up

--- src/App.js
+++ src/App.js
@@ -1,48 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 
+// fancy import syntax
+// will be explained later
+/**
+ * use state is called a "HOOKS" more will come later
+ * a "HOOK" is fancy tool used inside of react components
+ * means you tracking something across multiple calls to do something
+ * this is the same as saying import React from 'react'
+ * const useSate = React.useState;
+ */
+import { useState } from 'react';
+
 // if node
 // const HelloCullen = require('./HelloCullen');
 //in react
-import HelloCullen from './HelloCullen';
+//mport HelloCullen from './HelloCullen';
+// let drinkCount = 1;
 
-// app is what we call a component
-// react is made of components
-// components are functions that return JSX = HTML
 function App() {
+  // this useState has the value of 1
+  //["statevariable", how you change your state, using a function]
+  /**
+   * this is called "array destructuring"
+   */
+  //const returnValOfUseState = useState(1);
+  // the value of our current state
+  //const currentState = returnValOfUseState[0];
+  //
+  //const setDrinkCount = returnValOfUseState[1];
+  // USE THIS "USESTATE" SYNTAX
+  const [drinkCount, setDrinkCount] = useState(1);
+
   // embed variables in the HTML
   let name = 'Edan';
 
   let myH1 = <h1>go home, {name}, youre drunk!!!!</h1>;
   return (
-    <div className="App">
-      {myH1}
-      {/* HelloCullen is a React Compoennt */}
-      <header className="App-header">
-        <HelloCullen /> {/** self closing tag */}
-        <HelloCullen />
-        <HelloCullen />
-        <HelloCullen />
-        <HelloCullen />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload and save to win
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Cullen Happy Hour!</h1>
+      <p>
+        {name} had {drinkCount} drink
+      </p>
+
+      <button onClick={() => setDrinkCount(drinkCount + 1)}>Drink Up</button>
+
+      <button onClick={() => setDrinkCount(0)}>Sober Up</button>
     </div>
   );
 }
-
-// function HelloCullen() {
-//   return <h1>Hello Cullen</h1>;
-// }
-
 export default App;
